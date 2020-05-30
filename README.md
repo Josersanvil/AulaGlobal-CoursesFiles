f8bdd352f5df062d6f7df7438d231783e67b1f3f
jfverhook <juanfranciscoverhook@gmail.com>
2020-05-30 02:34:34 +0200

    Changed password and username creds to token only, so it's more secure

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # AulaGlobal-CoursesFiles
-Downloads all the files of all your courses of Aula Global into your machine using your NIA and password.
 
-## Requirements
+Programa para descargar todos los archivos de tus cursos en tu página de Moodle Aula Virtual a tu maquina
+
+Downloads all the files of all your courses of Moodle Aula Virtual into your machine.
+
+Instrucciones en español: [aqui](%Instrucciones)
+
+English Instructions: [aqui](%Use)
+
+## Use Example:
+```
+python download_AG_courses.py -t mysecuritytoken -d C:\Users\user\Documents\mycourses
+```
+
+## Requirements / Requerimientos:
+
 Python 3 or Python 2.7
 
 Download Python: https://www.python.org/downloads/
@@ -10,15 +23,40 @@ Download Python 3.7: https://www.python.org/downloads/release/python-372/
 
 Download Python 2.7: https://www.python.org/downloads/release/python-279/
 
-## Use
-Run the script "download_AG_courses.py" with the arguments: 
-- -u (Your Aula Global username [NIA]) 
-- -p (Your password) 
-- -d (Machine path to directory where files will be downloaded) [Optional Arg]
+##Instrucciones
 
-### Example:
-```
-python download_AG_courses.py -u 100123456 -p mypassword -d C:\Users\main\Documents\Curso 1\
-```
+Descarga el repositorio clockeando: `Clone or download > Download ZIP` y descomprimelo en tu maquina.
 
+Corre el programa en python con `download_AG_courses.py` pasando los siguientes argumentos:
+
+- -t : Tu token de seguridad de `aulaglobalmovil`, que es diferente a la contraseña que usas para entrar a la plataforma.
+- -d : Ruta a la carpeta en la que se guardaran los archivos descargados. **Opcional**. Se guardaran en `./cursos` por defecto.
+
+### Para conseguir el token de seguridad:
+
+Puedes conseguir tu clave de seguridad entrando a la plataforma de Aula Virtual con tu usuario y contraseña e ir a "Perfil" (Clickea tu nombre en la esquina superior derecha para ver el menú), despues abre el menu de configuracion en la esquina superior derecha de la página y clickea en "Claves de seguridad". Copia la clave que tiene `servicio: aulaglobalmovil`.
+
+> Si el token esta expirado solo crea uno nuevo en `Operación > Reiniciar`.
+
+##Use
+Download the repository by clicking: `Clone or download > Download ZIP` and then uncompress it.
+
+Run the script `download_AG_courses.py` with the arguments: 
+
+- -t Your personal `aulaglobalmovil` Security key, which is different from the password you use to log in to the platform.
+- -d Machine path to directory where files will be downloaded. Defaults to `./cursos` **Optional**
+
+### Get your token:
+
+You can get your security key token by logging in to Aula Virtual and going to "Profile" (If you are on the main page, just click your name and then open `Profile`), then open the configuration menu on the top right corner of the page and click "Security keys". Then copy the key that has `Service: aulaglobalmovil`
+
+> If the token is expired just click on `Operation > Reset`, It'll immediately create a new valid token.
+
+Then copy the key that says `service: aulaglobalmovil`. You can reset this key at any time if you want.
+
+## Example images to get security token:
+
+![Location of Profile on the main page](docs/Annotations-main_page.png)
+
+![Location of security keys on the profile settings page](docs/Annotations-profile_page.png)
 
--- download_AG_courses.py
+++ download_AG_courses.py
@@ -1,66 +1,65 @@
+"""
+Script to download all your couses contents from Aulaglobal to your machine.
+
+THE AUTHOR OR THE PROGRAM HAVE NO ASSOCIATION WITH UNIVERSIDAD CARLOS III DE MADRID OR ANY OF IT'S ASSOCIATES.
+THE PURPOSE OF THIS PROGRAM IS MERELY TO BE USED AS A FREE UTILITY WHICH FACILITATES THE STUDENTS, THE ACCESS
+TO THEIR COURSES CONTENTS IN AN OFFLINE MANNER. 
+
+For questions contact the user of the github repository:
+at: https://github.com/Josersanvil/AulaGlobal-PDF-download
+
+"""
 try:
-    # For Python 3.0 and later
-    import urllib.request as urllib_req
+	# For Python 3.0 and later
+	import urllib.request as urllib_req
 except ImportError:
-    # Fall back to Python 2's urllib_req
-    import urllib2 as urllib_req 
+	# Fall back to Python 2's urllib_req
+	import urllib2 as urllib_req 
 
 import json
 import os
 import argparse
 import xml.etree.ElementTree as et
 
+import requests
 
 
 domain = 'aulaglobal.uc3m.es'
 webservice = '/webservice/rest/server.php'
-service = 'ag_mobile'
+service = 'aulaglobal_mobile'
 
 
-# First we need the token
-
-def get_token(user, passwd):
-    url_token = 'https://' + domain + '/login/token.php?username=' \
-        + user + '&password=' + passwd + '&service=' + service
-    req = urllib_req.Request(url_token)
-    resp = urllib_req.urlopen(req).read()
-
-    # JSON :)
-
-    data = json.loads(resp.decode('utf8'))
-    token = data.get('token')
-
-    # Error, password / username wrong?
-
-    if token is None:
-        print(data.get('error'))
-        exit()
-    return token
-
-
-# Get the userid necessary to get user courses
-
+"""
+Get the userid necessary to get user courses
+"""
 def get_user_info(token):
-    url_info = 'https://' + domain + webservice + '?wstoken=' + token \
-        + '&wsfunction=core_webservice_get_site_info'
-    req = urllib_req.Request(url_info)
-    resp = urllib_req.urlopen(req).read()
+	url_info = 'https://' + domain + webservice + '?wstoken=' + token \
+		+ '&wsfunction=core_webservice_get_site_info'
+	req = urllib_req.Request(url_info)
+	resp = urllib_req.urlopen(req).read()
+	if "invalidtoken" in resp.decode():
+		print(
+			"Error: \nInvalid Token, the token may have expired or has a typo."
+			"\nCheck if it's written correctly or generate a new one in 'aulaglobal.uc3m.es'"
+		)
+		exit()
 
-    # Yes, is a XML
-
-    root = et.fromstring(resp)
-    name = root.find("SINGLE/KEY[@name='fullname']/VALUE")  # Name of the student
-    user_id = root.find("SINGLE/KEY[@name='userid']/VALUE").text
-    print("User ID: " + user_id + ", " + name.text)
-    return user_id
+	root = et.fromstring(resp)
+	name = root.find("SINGLE/KEY[@name='fullname']/VALUE")  # Name of the student
+	user_id = root.find("SINGLE/KEY[@name='userid']/VALUE").text
+	print("Your User ID: " + user_id + ", " + name.text)
+	return user_id
 
 
-# Just simply return a list of courses ids
 
+"""
+Return a list of courses ids
+"""
 def get_courses(token, userid):
 	url_courses = 'https://' + domain + webservice + '?wstoken=' \
 		+ token + '&wsfunction=core_enrol_get_users_courses&userid=' \
-	    + userid
+		+ userid
+
 	req = urllib_req.Request(url_courses)
 	resp = urllib_req.urlopen(req).read()
 
@@ -75,35 +74,37 @@ def get_courses(token, userid):
 			courses.append({"name":courses_names[i], "id":ids[i]})
 		else:
 			courses.append({"name":ids[i], "id":ids[i]})
-    
+	
 	return courses
 
 
-# Get the course contents (files urls)
 
+"""
+Get the course contents (files urls)
+"""
 def get_course_content(token, course_id):
-    url_course = 'https://' + domain + webservice + '?wstoken=' + token \
-        + '&wsfunction=core_course_get_contents&courseid=' + course_id
-        
-    req = urllib_req.Request(url_course)
-    resp = urllib_req.urlopen(req).read()
-    root = et.fromstring(resp)
-    xml_modules = "MULTIPLE/SINGLE/KEY[@name='modules']/MULTIPLE/"
-    xml_contents = "SINGLE/KEY[@name='contents']/MULTIPLE/SINGLE"
-    file_contents =  root.findall(xml_modules+xml_contents )  
-    files = []
-    for file_content in file_contents:
-        file_url = file_content.find("KEY[@name='fileurl']/VALUE").text
-        file_name = file_content.find("KEY[@name='filename']/VALUE"
-                ).text
-        file_type = file_content.find("KEY[@name='type']/VALUE").text
-        if file_type == 'file':
-            moodle_file = {}
-            moodle_file['file_name'] = file_name
-            moodle_file['file_url'] = file_url
-            files.append(moodle_file)
+	url_course = 'https://' + domain + webservice + '?wstoken=' + token \
+		+ '&wsfunction=core_course_get_contents&courseid=' + course_id
+	req = urllib_req.Request(url_course)
+	resp = urllib_req.urlopen(req).read()
+	root = et.fromstring(resp)
+	xml_modules = "MULTIPLE/SINGLE/KEY[@name='modules']/MULTIPLE/"
+	xml_contents = "SINGLE/KEY[@name='contents']/MULTIPLE/SINGLE"
+	file_contents =  root.findall(xml_modules+xml_contents )  
+	files = []
+	files.append({"file_name":"contents.xml","file_url":url_course})
+	for file_content in file_contents:
+		file_url = file_content.find("KEY[@name='fileurl']/VALUE").text
+		file_name = file_content.find("KEY[@name='filename']/VALUE"
+				).text
+		file_type = file_content.find("KEY[@name='type']/VALUE").text
+		if file_type == 'file':
+			moodle_file = {}
+			moodle_file['file_name'] = file_name
+			moodle_file['file_url'] = file_url
+			files.append(moodle_file)			
 
-    return files
+	return files
 
 
 
@@ -112,65 +113,67 @@ Saves the files of your aula global courses in the folder of your machine direct
 If not dirPath is specified a 'cursos' folder will be created in this script's parent directory.
 """
 def save_files(token, course_id, files, dirPath=None):
-    course_id = course_id.replace("/","_")
-    course_len = len(course_id)
+	course_id = course_id.replace("/","_")
+	course_len = len(course_id)
 
 	#Check if operative system is Windows (In windows if path is too long the cmd wont recognize it.)
-    if os.name == "nt" and course_len > 55: 
-        course_len = 55
+	if os.name == "nt" and course_len > 55: 
+		course_len = 55
 
-    if dirPath == None:
-        path = os.path.join(os.getcwd(),"cursos",course_id[:course_len-1])
-    else:
-        if not os.path.isdir(dirPath):
-            raise(Exception("The path given as argument is not a directory"))
-        path = os.path.join(dirPath, course_id[:course_len-1])
+	if dirPath == None:
+		path = os.path.join(os.getcwd(),"cursos",course_id[:course_len-1])
+	else:
+		if not os.path.isdir(dirPath):
+			os.makedirs(dirPath)
+		path = os.path.join(dirPath, course_id[:course_len-1])
 
-    if not os.path.exists(path):
-        os.makedirs(path)
+	if not os.path.exists(path):
+		os.makedirs(path)
 
-    for moodle_file in files:
-        url = (moodle_file['file_url'] + '&token=' + token)
-        file = os.path.join(path, moodle_file["file_name"].replace("/","_"))
-        print("\nDownloading file to: ", file)
+	n = 0
+	for moodle_file in files:
+		if n == 0:
+			# first file is the contentx.xml file:
+			url = moodle_file["file_url"]
+		else: 
+			url = (moodle_file['file_url'] + '&token=' + token)
+		file = os.path.join(path, moodle_file["file_name"].replace("/","_"))
+		print("\nDownloading file to: ", file)
 
-        try:
-            response = urllib_req.urlopen(url)
-        except Exception as e: # Most probabily an URLError (Exception for python 2 and 3 compatibility) [Lazy fix]
-            print("Couldn't download this file. \n%s"%(e))
+		try:
+			req = urllib_req.Request(url)
+			response = urllib_req.urlopen(req)
+		except Exception as e: # Most probabily an URLError (Exception for python 2 and 3 compatibility) [Lazy fix]
+			print("Couldn't download this file. \n%s"%(e))
 
-        with open(file, "wb") as f:
-            f.write(response.read())
+		with open(file, "wb") as f:
+			f.write(response.read())
 
 
 """
 Download all the files of the student's courses 
 """
 def main():
-    parser = \
-        argparse.ArgumentParser(description='Aula Global from  Command Line')
+	parser = \
+		argparse.ArgumentParser(description="Aula Global download files from  Command Line using 'aulaglobalmovil' Security key")
 
-    parser.add_argument('-u', metavar='User (NIA)', action='store',
-                        required=True)
-    parser.add_argument('-p', metavar='Password', action='store',
-                        required=True)
-    parser.add_argument('-d', metavar='Path for Output Directory (optional)', action='store',
-                        required=False)
-    args = parser.parse_args()
+	parser.add_argument('-t',metavar="User's 'aulaglobalmovil' token.", action='store', required=True)
+	parser.add_argument('-d', metavar="Path for Output Directory (optional)", action='store',
+						required=False)
+	args, _ = parser.parse_known_args()
 
-
-    token = get_token(args.u, args.p)
-    userid = get_user_info(token)
-    courses = get_courses(token, userid)
-    for course_id in courses:
-        print('Course: ' + course_id["name"].text)
-        files_url = get_course_content(token, course_id["id"].text)
-        if args.d:
-        	save_files(token, course_id["name"].text, files_url, args.d)
-        else:
-        	save_files(token, course_id["name"].text, files_url)
+	token = args.t
+	userid = get_user_info(token)
+	courses = get_courses(token, userid)
+	for course_id in courses:
+		print('Course: ' + course_id["name"].text)
+		files_url = get_course_content(token, course_id["id"].text)
+		if args.d:
+			save_files(token, course_id["name"].text, files_url, args.d)
+		else:
+			save_files(token, course_id["name"].text, files_url)
 
 
 if __name__ == '__main__':
-    main()
+	main()
 
